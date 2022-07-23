a91b8db31bc6caea48253a37e4e4ce961c12e178
Victor Kasap <kasap.victor@gmail.com>
2022-07-23 18:09:44 +0400

    fix: Fixed channel manage

--- app/src/pages/Home/components/Channels/components/ChannelsList/Channel/Channel.jsx
+++ app/src/pages/Home/components/Channels/components/ChannelsList/Channel/Channel.jsx
@@ -15,11 +15,12 @@ const ButtonChannelRemovable = ({
   return (
     <Dropdown as={ButtonGroup} className="w-100">
       <Button variant={variant} className="text-start w-100 text-truncate" onClick={onclick(id)}>
-    
         # {name}
       </Button>
-      <Dropdown.Toggle split variant={variant} className="flex-grow-0 text-end" />
+      
+      <Dropdown.Toggle split variant={variant} className="flex-grow-0 text-end" >
         <span className="visually-hidden">{t('channel.manage')}</span>
+      </Dropdown.Toggle>
       
       <Dropdown.Menu>
         <Dropdown.Item onClick={handleRemove(id)}>
