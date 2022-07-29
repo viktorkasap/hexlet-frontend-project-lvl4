88b070ceaf260d59ef040ca732b8e3576ebe25ca
Victor Kasap <kasap.victor@gmail.com>
2022-07-29 07:25:19 +0400

    fix: added width for notfound image

--- app/src/pages/NotFound/NotFound.jsx
+++ app/src/pages/NotFound/NotFound.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+import notfoundImg from './notfound.svg';
 
-const NotFound = () => (
-  <Container fluid className="h-100">
-    <header className="header">
-      <div className="container">
-        <div className="header__content">
-          <h1>404 - Page not found.</h1>
-        </div>
-      </div>
-    </header>
+const NotFound = () => {
+  const { t } = useTranslation();
 
-    <main className="main">
-      <div className="container">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, sed.</p>
-      </div>
-    </main>
+  return (
+    <Container fluid className="h-100">
+    <div className="text-center">
+      <img className="img-fluid h-25" src={notfoundImg} alt="notfound" style={{width: 300}}/>
+      <h1 className="h4 text-muted">{ t('notFound.header') }</h1>
+      <p className="text-muted">{ t('notFound.message') } <a href="/">{ t('notFound.linkText') }</a></p>
+    </div>
   </Container>
-);
+)};
 
 export default NotFound;
